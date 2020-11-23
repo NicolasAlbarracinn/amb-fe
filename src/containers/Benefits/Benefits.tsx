68ff359afa377504f237986b73c00eb3d6117443
Juan Pablo Lorek <jp92lorek@gmail.com>
2020-11-23 15:33:53 -0300

    feat: add first step for new benefits form

--- src/containers/Benefits/Benefits.tsx
+++ src/containers/Benefits/Benefits.tsx
@@ -4,10 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import BenefitsForm from 'pages/BenefitsPage/BenefitsForm';
 
-const Financing = () => (
+const Benefits = () => (
   <Switch>
-    <Route path="/app/financing/new" component={BenefitsForm} />
+    <Route path="/app/benefits/new" component={BenefitsForm} />
   </Switch>
 );
 
-export default Financing;
+export default Benefits;
