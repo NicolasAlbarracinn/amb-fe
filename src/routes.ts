6a0744a484e5d59513675e1863ef3035b60d09ea
Juan Pablo Lorek <jp92lorek@gmail.com>
2020-11-29 18:39:26 -0300

    feat: add route link on side bar

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import PeopleIcon from '@material-ui/icons/People';
 import ListIcon from '@material-ui/icons/List';
+import AllOut from '@material-ui/icons/AllOut';
 import Dashboard from 'pages/Dashboard/Dashboard';
 
 const dashRoutes = [
@@ -31,6 +32,21 @@ const dashRoutes = [
         mini: 'LS',
         component: ListIcon,
         layout: '/app/partners',
+      },
+    ],
+  },
+  {
+    collapse: true,
+    name: 'Prestaciones',
+    icon: AllOut,
+    state: 'pageCollapse',
+    views: [
+      {
+        path: '/new',
+        name: 'Crear Prestacion',
+        mini: 'CP',
+        component: AllOut,
+        layout: '/app/benefits',
       },
     ],
   },
